0b657b0815c8c269e457b90f4c2bc0d0c64beb90
Dmitrii <dimaeselidze@icloud.com>
2024-06-04 17:24:57 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Dmitrii Eselidze on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
