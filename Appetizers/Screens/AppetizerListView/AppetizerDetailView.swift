a55864482d4c35164ec8dbabf334ace0f93471d8
Dmitrii Eselidze <dmitry.eselidze@yandex.ru>
2024-06-09 22:39:23 +0300

    Detail View Realisation 1

---
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by Dmitrii Eselidze on 08.06.2024.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerDetailView()
+}
