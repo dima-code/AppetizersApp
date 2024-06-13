75c97fa30691c9d944c4810dfeb7dd8afc80a985
Dmitrii Eselidze <dmitry.eselidze@yandex.ru>
2024-06-14 00:21:35 +0300

    Order Empty State

---
+++ Appetizers/Views/Empty States/EmptyState.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyState.swift
+//  Appetizers
+//
+//  Created by Dmitrii Eselidze on 13.06.2024.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyState()
+}
